2875f24a83bd9488c1965c2fcdbed2832b42c21e
m29mujib <mamujib29@gmail.com>
2021-09-25 17:42:23 +0700

    Latihan2dan3

---
+++ application/views/view-form-matakuliah.php
@@ -0,0 +1,58 @@
+<html>
+
+<head>
+	<title>Form Input Matakuliah</title>
+</head>
+
+<body>
+	<center>
+		<form action="<?= base_url('matakuliah/cetak'); ?>" method="post">
+			<table class="table-hover">
+				<tr>
+					<th colspan="3">
+						Form Input Data Mata Kuliah
+					</th>
+				</tr>
+				<tr>
+					<td colspan="3">
+						<hr>
+					</td>
+				</tr>
+				<tr>
+					<th>Kode Mtk</th>
+					<th>:</th>
+					<td>
+						<input type="text" name="kode" id="kode" placeholder="Required">
+					</td>
+				</tr>
+				<tr>
+					<th>Nama Mtk</th>
+					<th>:</th>
+					<td>
+						<input type="text" name="nama" id="nama" placeholder="Required">
+					</td>
+				</tr>
+				<tr>
+					<th>SKS</th>
+					<th>:</th>
+					<td>
+						<select name="sks" id="sks">
+							<option value="">Pilihan SKS</option>
+							<option value="2">2</option>	
+							<option value="3">3</option>
+							<option value="4">4</option>	
+                        </select>
+                       </td>
+                   </tr>
+                   <tr>
+                   	<td colspan="3" align="center">
+                   		<input type="submit" value="submit">
+                   	</td>
+                   </tr>
+                  </table> 
+
+		</form>
+	</center>
+
+</body>
+</html>
---
+++ application/views/view-latihan1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Latihan 1</title>
+</head>
+<body>
+	Halo Kawan.. Yuk Kita belajar web programming.. !!!<br>
+	Nilai 1 = <?= $nilai1; ?> <br>
+	Nilai 2 = <?= $nilai2; ?> <br>
+	Ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " .
+	$nilai2 . " = " . $hasil; ?>
+
+</body>
+</html>
